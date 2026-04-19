0b0968345116942421d940d5bf5c5222ed6767d1
K M Santosh <santusantosh9785@gmail.com>
2026-04-19 19:23:22 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,18 @@
+temperature = 49
+if temperature >50:
+    print("dgpu need to use")
+elif temperature <48:
+    print("igpu is enough")
+else:
+    print("continue to use without change")
+    
+age = 17
+message = "eligible for marriage" if age  > 18 else "not eligible for marriage"
+print(message)
+if 10 == "10":
+    print("a")
+elif "bag">"apple" and "cat" > "ball":
+    print("b")
+else:
+    print("c")
+
