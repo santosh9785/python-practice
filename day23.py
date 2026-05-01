eb02c2c14f902a71af256fa9b0b9bfc0fb5cd740
K M Santosh <santusantosh9785@gmail.com>
2026-05-01 19:53:24 +0530

    Add files via upload

---
+++ day23.py
@@ -0,0 +1,11 @@
+name="santosh"
+print(name.upper())
+print(name.lower())
+print(name.title())
+print(name[6])
+print(name.find("t")) 
+print(name.replace("s","S"))
+print("sa" in name)
+print("sr" in name)
+new_name = name[:4] + "h" + name[4:]
+print(new_name)
