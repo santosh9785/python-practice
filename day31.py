3aee6e708c390c1bacb33877ceb895499da7bae8
K M Santosh <santusantosh9785@gmail.com>
2026-05-03 21:31:52 +0530

    Add files via upload

---
+++ day31.py
@@ -0,0 +1,21 @@
+for number in range(3):
+    print("santosh", number)
+
+for number in range(3):
+    print("santosh", number+1)
+
+
+for number in range(3):
+    print("santosh",number+1,(number+1)* ".")
+
+
+for  number in range(3):
+    print("santosh", number,number*".")
+
+
+for  number in range(1,6):
+    print("santosh", number,number*".")
+
+
+for  number in range(1,10,3):
+    print("santosh", number,number*".")
