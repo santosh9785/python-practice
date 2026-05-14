19c1bb120de46ef423e03d383cee84c472f4d308
K M Santosh <santusantosh9785@gmail.com>
2026-05-14 21:51:30 +0530

    Add files via upload

---
+++ grademarks.py
@@ -0,0 +1,15 @@
+n = int(input("enter student marks"))
+if 90 <= n <= 100:
+    print("ex")
+elif 80<= n < 90:
+    print("A")
+elif 70 <= n < 60:
+    print("B")
+elif 50 <= n <60:
+    print("C")
+elif 40 <= n <50:
+    print("D")
+else:
+    print(" failed and grade is :E")
+    n = int(input("enter student marks: "))
+
