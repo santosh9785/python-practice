8734e73600874ad929e33414574c13ca56f5084b
K M Santosh <santusantosh9785@gmail.com>
2026-05-15 21:09:24 +0530

    Add files via upload

---
+++ dictmethods.py
@@ -0,0 +1,12 @@
+list = {
+    "marks" : 100,
+    "age" : 19,
+    "rank" : 1,
+}
+print(list)
+print(list.items())
+print(list.keys())
+print(list.values())
+print(list.get("age"))
+print(list.clear())
+print(list.copy())
